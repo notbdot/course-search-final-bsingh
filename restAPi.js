b05094a2e100a1fb8e564a18bd79415e6fcfb6ff
notbdot <44618408+notbdot@users.noreply.github.com>
2020-12-06 14:23:08 -0500

    Update restAPi.js

--- restAPi.js
+++ restAPi.js
@@ -2,9 +2,6 @@ var PORT = process.env.PORT || 3000;
 var express = require("express");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
 var api_routes = require('./api_routes.js');
 app.use('/api', api_routes);
 
