4b11c9dbcdf374b41dc71a1feb26b6ed4d87e39f
notbdot <44618408+notbdot@users.noreply.github.com>
2020-12-06 14:45:46 -0500

    Update app_code.js

--- front_end/app_code.js
+++ front_end/app_code.js
@@ -1,7 +1,8 @@
+var port = location.port;
 var updateView = async (button) => {
     if (button.dataset.querytype == 'by_instructor') {
         let queryvalue = document.querySelector('#nameQuery').value;
-        api = `http://localhost:${PORT}/api/by_instructor/${queryvalue}`;
+        api = `http://localhost:${port}/api/by_instructor/${queryvalue}`;
     }
     else if (button.dataset.querytype == 'by_code') {
         let queryvalue = document.querySelector('#codeQuery').value;
