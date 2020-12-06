be0f868be12981f36d20396d1b1996fe9d880493
notbdot <44618408+notbdot@users.noreply.github.com>
2020-12-06 14:36:30 -0500

    Update app_code.js

--- front_end/app_code.js
+++ front_end/app_code.js
@@ -1,24 +1,24 @@
 var updateView = async (button) => {
     if (button.dataset.querytype == 'by_instructor') {
         let queryvalue = document.querySelector('#nameQuery').value;
-        api = `http://localhost:PORT/api/by_instructor/${queryvalue}`;
+        api = `http://localhost:3000/api/by_instructor/${queryvalue}`;
     }
     else if (button.dataset.querytype == 'by_code') {
         let queryvalue = document.querySelector('#codeQuery').value;
-        api = `http://localhost:PORT/api/by_course_code/${queryvalue}`;
+        api = `http://localhost:3000/api/by_course_code/${queryvalue}`;
     }
     else if (button.dataset.querytype == 'by_title') {
         let queryvalue = document.querySelector('#titleQuery').value;
-        api = `http://localhost:PORT/api/by_title/${queryvalue}`;
+        api = `http://localhost:3000/api/by_title/${queryvalue}`;
     }
     else if (button.dataset.querytype == 'by_level') {
         let queryvalue = document.querySelector('#levelQuery').value;
-        api = `http://localhost:PORT/api/by_level/${queryvalue}`;
+        api = `http://localhost:3000/api/by_level/${queryvalue}`;
     }
     else if (button.dataset.querytype == 'combined_query') {
         let combinedIns = document.querySelector('#combinedIns').value;
         let combinedLev = document.querySelector('#combinedLev').value;
-        api = `http://localhost:PORT/api/combined_query/${combinedIns}/${combinedLev}`;
+        api = `http://localhost:3000/api/combined_query/${combinedIns}/${combinedLev}`;
     }
 
     const data = await fetch(api);
