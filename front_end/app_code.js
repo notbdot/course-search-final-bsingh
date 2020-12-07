ebf72df6f1104aba4561368ee881945e1ac29f1d
Brandon Singh <brandonanthonysingh@gmail.com>
2020-12-06 22:40:43 -0500

    fetch

--- front_end/app_code.js
+++ front_end/app_code.js
@@ -1,4 +1,5 @@
 var URL = window.location.hostname;
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 var updateView = async (button) => {
     if (button.dataset.querytype == 'by_instructor') {
         let queryvalue = document.querySelector('#nameQuery').value;
@@ -22,7 +23,18 @@ var updateView = async (button) => {
         api = `https://course-search-final.herokuapp.com/api/combined_query/${combinedIns}/${combinedLev}`;
     }
 
-    const data = await fetch(api);
+    const data = await fetch(proxyUrl + api)
+    .then(blob => blob.json())
+    .then(data => {
+      console.table(data);
+      document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+      return data;
+    })
+    .catch(e => {
+      console.log(e);
+      return e;
+    });
+    
     const model = await data.json();
 
     render_view(model);
@@ -34,20 +46,3 @@ var render_view = (model) => {
 
     document.querySelector("#results").innerHTML = html;
 }
-
-(function() {
-    var cors_api_host = 'cors-anywhere.herokuapp.com';
-    var cors_api_url = 'https://' + cors_api_host + '/';
-    var slice = [].slice;
-    var origin = window.location.protocol + '//' + window.location.host;
-    var open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function() {
-        var args = slice.call(arguments);
-        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-            targetOrigin[1] !== cors_api_host) {
-            args[1] = cors_api_url + args[1];
-        }
-        return open.apply(this, args);
-    };
-})();
